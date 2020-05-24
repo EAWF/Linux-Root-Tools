d32566a14961356f6f3cbc9d2e74250000c648df
Bob Holden <github@eawf.com>
2020-05-24 09:21:02 -0700

    Update allinone.sh

--- ServerInit/allinone.sh
+++ ServerInit/allinone.sh
@@ -1,1 +1,10 @@
+#! /bin/bash
+# Installation/Configuration Script for NEW CentOS 7 Minimal VPS.
+yum -y upgrade
+yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
+yum -y install epel-release yum-utils
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum -y install centos-release-scl.noarch
+yum -y upgrade
 
