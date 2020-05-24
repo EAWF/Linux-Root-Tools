0bee24adc48e2a5518c40e16ed44b7fcb46c8d84
Bob Holden <github@eawf.com>
2020-05-24 09:26:47 -0700

    Update allinone.sh

--- ServerInit/allinone.sh
+++ ServerInit/allinone.sh
@@ -11,12 +11,15 @@ yum -y install man mlocate nano net-tools rsyslog sudo which whois ImageMagick v
 yum -y install perl perl-GDGraph perl-libwww-perl.noarch perl-LWP-Protocol-https.noarch
 yum -y install mutt swaks rkhunter curl wget file expect
 cd /usr/src
-rm -fv csf.tgz
 wget https://download.configserver.com/csf.tgz
 tar -xzf csf.tgz
 cd csf
 sh install.sh
 cd ~
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install yum-utils
+yum-config-manager --enable remi-php74
+yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-gmp php-base58
+php -v
 yum -y upgrade
-
-
