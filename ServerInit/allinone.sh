a923248efbb7fdf97ccba2d540c77fded7a57df2
Bob Holden <github@eawf.com>
2020-05-24 09:22:05 -0700

    Update allinone.sh

--- ServerInit/allinone.sh
+++ ServerInit/allinone.sh
@@ -6,5 +6,8 @@ yum -y install epel-release yum-utils
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 yum -y install centos-release-scl.noarch
-yum -y upgrade
+yum -y install bind-utils cronie gcc ipset iptables-services lynx make e2fsprogs gd
+yum -y install man mlocate nano net-tools rsyslog sudo which whois ImageMagick vim
+yum -y install perl perl-GDGraph perl-libwww-perl.noarch perl-LWP-Protocol-https.noarch
+yum -y install mutt swaks rkhunter curl wget file expect
 
