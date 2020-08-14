e7d3e796bbf94f4996474148a91b59b64ed378de
Bob Holden <github@eawf.com>
2020-08-14 10:09:07 -0700

    Update init.sh

--- ServerInit/init.sh
+++ ServerInit/init.sh
@@ -4,6 +4,6 @@
 yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 yum -y install epel-release yum-utils
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+// rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm For PHP 7.1. See PHP installation section
 yum -y install centos-release-scl.noarch
 yum -y upgrade
