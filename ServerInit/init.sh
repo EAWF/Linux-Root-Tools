e4ae5107b1af4f0c59d965071797ab83d3eacefa
Bob Holden <github@eawf.com>
2021-06-17 08:29:37 -0700

    Update init.sh

--- ServerInit/init.sh
+++ ServerInit/init.sh
@@ -1,5 +1,5 @@
-# This script does not support PHP 8+, which requires using dnf to update.  Will update this code sometime soon.  RH
-
+System Initialization Script
+```bash
 #!/bin/bash
 # EAWF CentOS7 Initialization Script
 # Run this on a new CentOS 7 Minimal Install
@@ -8,3 +8,6 @@ yum -y install epel-release yum-utils
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install centos-release-scl.noarch
 yum -y upgrade
+yum -y install dnf
+dnf -u
+```
