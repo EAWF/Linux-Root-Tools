ad50f070c2c6fd14980a87bf41d6b258f62615c8
Bob Holden <github@eawf.com>
2023-03-13 11:03:46 -0700

    Update init.sh

--- ServerInit/init.sh
+++ ServerInit/init.sh
@@ -3,6 +3,7 @@ System Initialization Script
 #!/bin/bash
 # EAWF CentOS7 Initialization Script
 # Run this on a new CentOS 7 Minimal Install
+* change all below for DNF and whatever version of Linux you have selected.
 yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 yum -y install epel-release yum-utils
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
